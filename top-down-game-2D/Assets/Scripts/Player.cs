81ef871b722df351ecfdf039b73c383d511c72df
juliana1390 <90989716+juliana1390@users.noreply.github.com>
2023-03-13 21:23:13 -0300

    animation: run

--- top-down-game-2D/Assets/Scripts/Player.cs
+++ top-down-game-2D/Assets/Scripts/Player.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
 
     private Rigidbody2D rig;
+
+    private float initSpeed;
+    private bool _isRunning;
     private Vector2 _direction;
     
     public Vector2 direction
@@ -14,20 +18,55 @@ public class Player : MonoBehaviour
         get{return _direction; }
         set{_direction = value; }
     }
-  
 
+    public bool isRunning
+    {
+        get{return _isRunning; }
+        set{_isRunning = value; }
+    }
+  
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        initSpeed = speed;
     }
 
     private void Update()
     {
-        _direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        OnInput();
+        OnRun();
     }
 
     private void FixedUpdate()
     {
+        OnMove();
+    }
+
+    #region Movement
+    
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void OnMove()
+    {
         rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
     }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initSpeed;
+            _isRunning = false;
+        }
+    }
+
+    #endregion    
 }
--- top-down-game-2D/Assets/Scripts/PlayerAnim.cs
+++ top-down-game-2D/Assets/Scripts/PlayerAnim.cs
@@ -15,6 +15,14 @@ public class PlayerAnim : MonoBehaviour
 
     void Update()
     {
+        OnMove();
+        OnRun();
+    }
+
+    #region Movement
+
+    void OnMove()
+    {
         if(player.direction.sqrMagnitude > 0)
         {
            anim.SetInteger("transition", 1); 
@@ -33,4 +41,14 @@ public class PlayerAnim : MonoBehaviour
             transform.eulerAngles = new Vector2(0, 180);
         }
     }
+
+    void OnRun()
+    {
+        if(player.isRunning)
+        {
+            anim.SetInteger("transition", 2);
+        }
+    }
+
+    #endregion
 }
